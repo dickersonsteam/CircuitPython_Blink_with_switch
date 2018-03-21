bb35e0fab17ba3b30a173a0c0c294d26417b0d41
dickersonsteam <37583333+dickersonsteam@users.noreply.github.com>
2018-03-21 12:16:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # main.py
-# CircuitPython Blink
+# CircuitPython Blink with Switch
 
 import time
-from digitalio import DigitalInOut, Direction
+from digitalio import DigitalInOut, Direction, Pull
 import board
 
 led = DigitalInOut(board.D6)
 led.direction = Direction.OUTPUT
 
+switch = DigitalInOut(board.D9). # use pin D9
+switch.direction = Direction.INPUT
+switch.pull = Pull.DOWN
+
 while True:
-    led.value = False
-    time.sleep(0.5) # delay half second
-    led.value = True
-    time.sleep(0.5)
+    if switch.value:  # if switch.value = true
+        led.value = False
+    else:  # all other possibilities
+        led.value = True
+        
+    time.sleep(0.01). # Debounce switch
